be723f1d4bc17afbd12195535d034610ea1419f4
Shane Fitzpatrick <fitzpatrick.shane@gmail.com>
2021-11-23 13:01:13 +0000

    Lab1 donation completed

---
+++ app/src/main/java/ie/wit/donationx/models/DonationModel.kt
@@ -0,0 +1,9 @@
+package ie.wit.donationx.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class DonationModel(var id: Long = 0,
+                         val paymentmethod: String = "N/A",
+                         val amount: Int = 0) : Parcelable
